7ebccf91e4bf76829637b6f3cf8e4921a3d684ce
Unknown <AlixAkhribi@yahoo.com>
2019-02-01 21:46:06 -0800

    Create VideoDetail component, display video title, description, UI/UX

---
+++ src/components/VideoDetail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="video">
+    <div class="video-details">
+      <h4>{{videoTitle}}</h4>
+      <p>{{videoDescription}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoDetail",
+  props: ["video"],
+  computed: {
+    videoTitle() {
+      return this.video.snippet.title;
+    },
+    videoDescription() {
+      return this.video.snippet.description;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .video-details{
+    margin-top: 1rem;
+    padding: 1rem;
+    border: .1rem solid #dddddd;
+    border-radius: .4rem;
+  }
+</style>

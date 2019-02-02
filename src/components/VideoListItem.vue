0a32aa1cffbbde53b9683a3fee161ef20209438c
Unknown <AlixAkhribi@yahoo.com>
2019-02-01 18:10:15 -0800

    Pass in video thumbnail and title to VideoListItem and UI/UX

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,18 +1,40 @@
 <template>
-    <div>
-        <li>
-            {{video.snippet.title}}
-        </li>
-    </div>
+  <div>
+    <li class="list-group-item media">
+      <img class="video-thumbail mr-3" :src="smallThumbnailURL">
+      <div class="media-body">
+        <p class="video-title">{{videoTitle}}</p>
+      </div>
+    </li>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'VideoListItem',
-    props: ['video']
-}
+  name: "VideoListItem",
+  props: ["video"],
+  computed: {
+    smallThumbnailURL() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+    videoTitle() {
+      return this.video.snippet.title;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.list-group-item {
+  display: flex;
+  cursor: pointer;
 
+  &:hover {
+    background: #eeeeee;
+  }
+
+  .video-title {
+    font-size: 1.6rem;
+  }
+}
 </style>

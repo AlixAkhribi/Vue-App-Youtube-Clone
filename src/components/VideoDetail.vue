8a47076c9cc14b4b06ba59af01b6b5576967a59f
Unknown <AlixAkhribi@yahoo.com>
2019-02-03 17:40:25 -0800

    Get video url to play selected video

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
   <div v-if="video">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" allowfullscreen :src="videoUrl"/>
+    </div>
     <div class="video-details">
       <h4>{{videoTitle}}</h4>
       <p>{{videoDescription}}</p>
@@ -17,16 +20,20 @@ export default {
     },
     videoDescription() {
       return this.video.snippet.description;
+    },
+    videoUrl() {
+      const { videoId } = this.video.id;
+      return `https://www.youtube.com/embed/${videoId}`;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-  .video-details{
-    margin-top: 1rem;
-    padding: 1rem;
-    border: .1rem solid #dddddd;
-    border-radius: .4rem;
-  }
+.video-details {
+  margin-top: 1rem;
+  padding: 1rem;
+  border: 0.1rem solid #dddddd;
+  border-radius: 0.4rem;
+}
 </style>
